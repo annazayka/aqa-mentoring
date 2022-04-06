09ee3070ac77120940d4be3c6678d6b2ef9eab8c
Hanna <hanna@adminpdfs-MacBook-Air-2.local>
2022-04-06 18:45:59 +0300

    homework add 3th task

--- src/main/java/HomeworkClass.java
+++ src/main/java/HomeworkClass.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class HomeworkClass {
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HomeworkClass {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextFloat()) {
+            System.out.println(floatChar(sc.nextFloat()));
+            System.out.println(floatCharInt(sc.nextFloat()));
+        } else {
+            char inputInt = sc.next().charAt(0);
+            charBite(inputInt);
+        }
+    }
+
+    public static byte charBite(char charValue) {
+        return (byte) charValue;
+    }
+
+    public static char floatChar(float floatValue) {
+        return (char) floatValue;
+    }
+
+    public static int floatCharInt(float floatValue) {
+        return (int) (char) floatValue;
+    }
 }
